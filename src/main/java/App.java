22d2236a23843fb38eb57b5d1659e4d83da27989
Бабулех <dmitriy.lipenchuk.01@inbox.ru>
2022-06-16 15:06:30 +0300

    Task completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld secondBean = applicationContext.getBean(HelloWorld.class);
+
+        Cat catBean = applicationContext.getBean(Cat.class);
+        Cat secondCatBean = applicationContext.getBean(Cat.class);
+
+        System.out.println(bean == secondBean);
+        System.out.println(catBean == secondCatBean);
     }
 }
